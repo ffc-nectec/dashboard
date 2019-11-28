18b3765d093e21ea94312907ef22ac64c358590a
tawat chai <tawat1838@gmail.com>
2019-11-28 10:44:33 +0700

    Updates

--- precache-manifest.a28627873bd327607337d27043e90d26.js
+++ precache-manifest.a28627873bd327607337d27043e90d26.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ea546fd7dae93ef39823e67455cb00b",
+    "revision": "a997cadabab931eda1ad13d6514bd4e3",
     "url": "/dashboard/index.html"
   },
   {
@@ -24,15 +24,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dashboard/static/js/3.2bb0b945.chunk.js"
   },
   {
-    "revision": "9d60e84b9afa4a7ef9e7",
-    "url": "/dashboard/static/js/4.dd002eca.chunk.js"
+    "revision": "5d8402176d3fd69f5d7e",
+    "url": "/dashboard/static/js/4.b3b744a6.chunk.js"
   },
   {
     "revision": "c7521e1baff11d9ffa4a",
     "url": "/dashboard/static/js/main.6fa605ca.chunk.js"
   },
   {
-    "revision": "e60fe44a431abd2cac79",
-    "url": "/dashboard/static/js/runtime~main.c309cbfd.js"
+    "revision": "a99fe2ef734eca1376d7",
+    "url": "/dashboard/static/js/runtime~main.1528290a.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.1f6f526ca267006daea60a99a6ee6fca.js"
+  "/dashboard/precache-manifest.a28627873bd327607337d27043e90d26.js"
 );
 
 self.addEventListener('message', (event) => {
