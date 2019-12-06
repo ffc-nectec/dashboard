bc5ed8b1683f22b87a376cf0297b9d7d862e94ca
tawat chai <tawat1838@gmail.com>
2019-12-06 15:10:51 +0700

    แก้ไขจำนวนทั้งหมดนิดหน่อย

--- src/components/widgets/Charts/ADLsuccess/index.js
+++ src/components/widgets/Charts/ADLsuccess/index.js
@@ -6,7 +6,10 @@ class ADLsuccess extends React.Component {
     console.log(pyramid60up, user, 'รักนะ');
     const active = user.MID + user.OK + user.VERYHI
     const total = pyramid60up.total60up
-    const sumtotal = active / total * 100
+    
+    let sumtotal = active / total * 100
+    if (Number.isNaN (sumtotal)) { sumtotal = 0 }
+
     let textClass = "text-success font-weight-bold font-size-24 mb-0"
     if (sumtotal < 50) {
       textClass = "text-danger font-weight-bold font-size-24 mb-0"
@@ -16,11 +19,13 @@ class ADLsuccess extends React.Component {
       <div className="d-flex flex-wrap align-items-center">
         <div className="mr-auto">
           <p className="text-uppercase text-dark font-weight-bold font-size-18 mb-1">ผู้สูงอายุที่ถูกประเมิน ADL แล้ว</p>
+          {pyramid60up.total60up &&
+            <p>(ทั้งหมดจำนวน {pyramid60up.total60up.toLocaleString()} คน)</p>
+          }
         </div>
-        <p className={textClass}>{sumtotal.toFixed(2)} %</p>
-        {pyramid60up.total60up &&
-          <p>(ทั้งหมดจำนวน {pyramid60up.total60up.toLocaleString()} คน)</p>
-        }
+        {(sumtotal > 0) && (
+          <p className={textClass}>{sumtotal.toFixed(2)} % </p>
+        )}
       </div>
     )
   }
